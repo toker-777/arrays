7602d928af3295c4a346c65c757e79945a724f6f
toker-777 <toker_777@bk.ru>
2024-09-27 00:30:23 +0300

    random arrays

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,3 @@
-import java.lang.reflect.Array;
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
         task1();
@@ -9,102 +6,66 @@ public class Main {
         task4();
     }
 
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
+    }
+
     public static void task1() {
         System.out.println("Задание 1");
 
-        byte[] byteArr = new byte[3];
-        float[] floatArr = {1.57f, 7.654f, 9.986f};
-        int[] intArr = new int[6];
-        byteArr[0] = 1;
-        byteArr[1] = 2;
-        byteArr[2] = 3;
-        intArr[0] = 10;
-        intArr[1] = 20;
-        intArr[2] = 30;
-        intArr[3] = 40;
-        intArr[4] = 50;
-        intArr[5] = 60;
-        System.out.print(byteArr[0] + " ");
-        System.out.print(byteArr[1] + " ");
-        System.out.println(byteArr[2] + " ");
-        System.out.print(floatArr[0] + " ");
-        System.out.print(floatArr[1] + " ");
-        System.out.println(floatArr[2] + " ");
-        System.out.print(intArr[0] + " ");
-        System.out.print(intArr[1] + " ");
-        System.out.print(intArr[2] + " ");
-        System.out.print(intArr[3] + " ");
-        System.out.print(intArr[4] + " ");
-        System.out.println(intArr[5] + " ");
+        int[] arr = generateRandomArray();
+        int allPayments = 0;
+        for (int i = 0; i < arr.length - 1; i++) {
+            int payments = arr[i];
+            allPayments = payments + allPayments;
+        }
+        System.out.println("Сумма трат за месяц составила " + allPayments + " рублей");
     }
 
     public static void task2() {
         System.out.println("Задание 2");
 
-        byte[] byteArr = new byte[3];
-        float[] floatArr = {1.57f, 7.654f, 9.986f};
-        int[] intArr = new int[6];
-        byteArr[0] = 1;
-        byteArr[1] = 2;
-        byteArr[2] = 3;
-        intArr[0] = 10;
-        intArr[1] = 20;
-        intArr[2] = 30;
-        intArr[3] = 40;
-        intArr[4] = 50;
-        intArr[5] = 60;
-        System.out.println(Arrays.toString(byteArr));
-        System.out.println(Arrays.toString(floatArr));
-        System.out.println(Arrays.toString(intArr));
+        int[] arr = generateRandomArray();
+        int maxPayments = 99_999;
+        int minPayments = 200_001;
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (maxPayments < arr[i]) {
+                maxPayments = arr[i];
+            }
+            if (minPayments > arr[i]) {
+                minPayments = arr[i];
+            }
+        }
+        System.out.println("Минимальная сумма трат за день составила " + minPayments + " рублей");
+        System.out.println("Максимальная сумма трат за день составила " + maxPayments + " рублей");
     }
 
     public static void task3() {
         System.out.println("Задание 3");
 
-        byte[] byteArr = new byte[3];
-        float[] floatArr = {1.57f, 7.654f, 9.986f};
-        int[] intArr = new int[6];
-        byteArr[0] = 1;
-        byteArr[1] = 2;
-        byteArr[2] = 3;
-        intArr[0] = 10;
-        intArr[1] = 20;
-        intArr[2] = 30;
-        intArr[3] = 40;
-        intArr[4] = 50;
-        intArr[5] = 60;
-        System.out.print(byteArr[2] + ", ");
-        System.out.print(byteArr[1] + ", ");
-        System.out.println(byteArr[0] + " ");
-        System.out.print(floatArr[2] + ", ");
-        System.out.print(floatArr[1] + ", ");
-        System.out.println(floatArr[0] + " ");
-        System.out.print(intArr[5] + ", ");
-        System.out.print(intArr[4] + ", ");
-        System.out.print(intArr[3] + ", ");
-        System.out.print(intArr[2] + ", ");
-        System.out.print(intArr[1] + ", ");
-        System.out.println(intArr[0] + " ");
+        int[] arr = generateRandomArray();
+        int allPayments = 0;
+        for (int payments = 0; payments < arr.length - 1; payments++) {
+            allPayments = allPayments + arr[payments];
+        }
+        System.out.println("Средняя сумма трат за месяц составила " + (float) allPayments / arr.length + " рублей");
     }
 
     public static void task4() {
         System.out.println("Задание 4");
 
-        byte[] byteArr = new byte[3];
-        byteArr[0] = 1;
-        byteArr[1] = 2;
-        byteArr[2] = 3;
-        for (byte i = 0; i < byteArr.length; i++) {
-            if (byteArr[i] % 2 != 0) {
-                if (i == byteArr.length - 1) {
-                    System.out.println(byteArr[i] + 1 + " ");
-                    break;
-                }
-                System.out.print(byteArr[i] + 1 + ", ");
-            } else {
-                System.out.print(byteArr[i] + ", ");
+        char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (int i = (reverseFullName.length - 1); i >= 0; i--) {
+            if (i == 0) {
+                System.out.println(reverseFullName[i]);
+                break;
             }
+            System.out.print(reverseFullName[i]);
         }
     }
 }
-
